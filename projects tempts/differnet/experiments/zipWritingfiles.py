cbb42dad9585ccbfed278cd55bfa501e762bef86
Naveed Zaynali <promisezey@gmail.com>
2023-06-30 05:49:02 +0330

    pip install PySimpleGUI

--- projects tempts/differnet/experiments/zipWritingfiles.py
+++ projects tempts/differnet/experiments/zipWritingfiles.py
@@ -4,6 +4,17 @@ def write_files(filepath, lists):
             file_locally.writelines(lists)
 
 
+def write_file(filepath, list):
+    with open(filepath, 'r') as local_file:
+        local_file.writelines(list)
+
+
+def filereader(filepath, items):
+    with open(filepath, 'r') as local_file:
+        items = local_file.readlines()
+        return items
+
+
 if __name__ == '__main__':
     job = []
     new = input('enter:')
